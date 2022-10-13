61b2713db2a61a767f7a651c440ae271acb16d1f
Andele97 <andreapinna40@live.it>
2022-10-13 15:57:04 +0200

    [finish]

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // ELEMENTI E DATI
 
 const container = document.querySelector('.container');
-const limit = 100;
+const limit = 200;
 
 // EVENTI
 
@@ -9,14 +9,22 @@ for(let i = 1; i <= limit; i++){
   const box = document.createElement('div');
   box.className = 'box';
 
-  if(i % 3){
-    box.classList.add('rosa-chiaro')
-  }else if(i % 5){
-    box.classList.add('rosa')
-  }else(i % 10){
-    box.classList.add('rossetto')
+  if(i % 15 == 0){
+    box.classList.add('rossetto');
+    box.innerHTML = 'FizzBozz';
+    container.append(box);
+    console.log('buzz');
+  }else if(i % 5 == 0){
+    box.classList.add('rosa');
+    container.append(box);
+    box.innerHTML = 'Buzz';
+  }else if(i % 3 == 0){
+    box.classList.add('rosa-chiaro');
+    container.append(box);
+    box.innerHTML = 'Fizz';
+    console.log('fizz');
+  }else{
+    box.innerHTML = i;
+    container.append(box);
   }
 };
-
-box.innerHTML = i;
-container.append(box);
